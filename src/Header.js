0ee88f239e573890ce0446fca31b62118281ac1b
Faruq Bakare <bakarefaruq.a@gmail.com>
2020-11-28 15:27:39 +0100

    init

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <h2>
+        I am a <header></header>
+      </h2>
+    </div>
+  );
+}
+
+export default Header;
+<h2>
+  I am a <header></header>
+</h2>;
