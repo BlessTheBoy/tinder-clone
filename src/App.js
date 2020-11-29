b0a3e63b12e34bd56915c525c67f99ae6a6e076e
Faruq Bakare <bakarefaruq.a@gmail.com>
2020-11-29 02:56:46 +0100

    jknkl

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import "./App.css";
 import Header from "./Header";
 import logo from "./logo.png";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import TinderCards from "./TinderCards";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <img src={logo} alt="" />
-
       <Header />
+      <Router>
+        <Switch>
+          <Route path="/chat">
+            <h1>Chat page</h1>
+          </Route>
+          <Route path="/">
+            <TinderCards />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/TinderCards.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import TinderCard from "react-tinder-card";
+import "./TinderCards.css";
+
+function TinderCards() {
+  const [people, setPeople] = useState([
+    {
+      name: "Steve Jobs",
+      url:
+        "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTY2MzU3OTcxMTUwODQxNTM1/steve-jobs--david-paul-morrisbloomberg-via-getty-images.jpg",
+    },
+    {
+      name: "Mark Zukerberg",
+      url:
+        "https://i.insider.com/5d84ce522e22af4c215743c6?width=1136&format=jpeg",
+    },
+  ]);
+  return (
+    <div>
+      <h1>TinderCards</h1>
+      <div className="tinderCards__cardContainer">
+        {people.map((person) => (
+          <TinderCard
+            className="swipe"
+            key={person.name}
+            preventSwipe={["up", "down"]}>
+            <div
+              style={{ backgroundImage: `url(${person.url})` }}
+              className="card">
+              <h3>{person.name}</h3>
+            </div>
+          </TinderCard>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default TinderCards;
